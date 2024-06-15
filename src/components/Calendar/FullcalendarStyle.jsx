3b9c0ffa1321f65077c2fccdb36dc6742331ac7b
kimseryeong <kxxoo322@gmail.com>
2024-06-15 22:05:58 +0900

    style: 캘린더 글씨 색상 수정

--- src/components/Calendar/FullcalendarStyle.jsx
+++ src/components/Calendar/FullcalendarStyle.jsx
@@ -41,6 +41,16 @@ export const FullCalendarStyle = styled.div`
     .fc-direction-ltr .fc-toolbar > * > :not(:first-child) {
         margin-left: 5px;
     }
-
     //button css ------------------------------
+    
+    .fc-daygrid-block-event {    
+        border: none;
+    }
+    .fc-event-main {
+        font-size: 12px;
+        color: black;
+    }
+    .fc-event-title{
+        color: black;
+    }
 `;
