5dc73d8b52b29cc5e0d350be3982317478019fb7
kimseryeong <kxxoo322@gmail.com>
2024-07-10 16:07:12 +0900

    chore: 불필요한 코드 삭제

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,34 +1,15 @@
-import './Contents.css';
 import Calendar from './Calendar/Calendar';
 import TodoTemplate from './Todo/TodoTemplate';
-import { useSetRecoilState, useRecoilValue } from 'recoil';
-import { userState, allTodosState } from '../lib/atom';
-import { fetchAllTodos } from '../API';
-import { useEffect } from 'react';
 import styled from 'styled-components';
 
 const ContentsStyle = styled.div`
     height: 100vh;
     display: flex;
     padding-top: 60px;
+    margin: 0 50px;
 `;
 
 export default function Contents () {
-
-    
-    const userInfo = useRecoilValue(userState);
-    const uuid = userInfo ? userInfo.user.id : null;
-    
-    // const setAllTodos = useSetRecoilState(allTodosState);
-    // useEffect(()=>{
-    //     const loadAllTodos = async () => {
-    //         const fetchedTodos = await fetchAllTodos(uuid);
-    //         setAllTodos(fetchedTodos);
-    //     }
-
-    //     loadAllTodos();
-    // }, [uuid, setAllTodos])
-
 
     return (
         <ContentsStyle>
