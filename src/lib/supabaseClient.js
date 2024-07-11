7946d5282944d5e5458766afa4dbad8ed41bcd64
kimseryeong <kxxoo322@gmail.com>
2024-07-11 17:41:23 +0900

    chore: supabase 사용자 로그인/로그아웃 세션정보 sessionStorage 로 변경

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,6 +2,14 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://fxssyslwcbucsbshilrr.supabase.co';
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-const client = createClient(supabaseUrl, supabaseKey);
+const client = createClient(supabaseUrl, supabaseKey, {
+    
+    auth: {
+        storage: sessionStorage,  // sessionStorage 사용
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true
+    }
+});
 
 export { client as supabase };
