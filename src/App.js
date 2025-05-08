28a53f7054271eaba732a8daf5bfc34fe5c11127
kimseryeong <kxxoo322@gmail.com>
2025-05-09 00:15:27 +0900

    style: create global style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import Header from './components/Header';
 import Contents from './components/Contents';
 import { RecoilRoot, RecoilEnv } from 'recoil';
 import { SessionProvider } from './components/SessionProvider';
-
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
@@ -12,8 +14,11 @@ const App = () => {
     <div className='App'>
       <RecoilRoot>
         <SessionProvider>
-          <Header/>
-          <Contents/>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Header/>
+            <Contents/>
+          </ThemeProvider>
         </SessionProvider>
       </RecoilRoot>
     </div>
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    colors: {
+        primary: "#bee1f7",
+        light: "#EAF2F8",
+        strong: "#A9CCE3",
+    },
+    fontSizes: {
+        sm: "14px",
+        md: "18px",
+        lg: "25px",
+    },
+    margins: {
+        desktop: "40px 150px",
+        tablet: "30px 80px",
+        mobile: "20px 45px",
+    },
+};
