06535b5ecc415c710c4ae8255d93bc2be51dc488
kimseryeong <kxxoo322@gmail.com>
2025-05-12 15:18:07 +0900

    chore: 로그인 후 사용자 정보, 구글 로그인 로고 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import Logout from './Logout';
 import styled from 'styled-components';
 import { useSession } from './SessionProvider';
+import { ReactSVG } from "react-svg";
 
 const HeaderStyle = styled.div`
     width: 100%;
-    height: 30px;
+    height: 40px;
     position: fixed;
     display: flex;
     font-family: 'pretendard';
@@ -18,10 +19,70 @@ const UserInfo = styled.div`
 `;
 
 const EmailStyle = styled.span`
-    font-size: 18px;
+    font-size: 16px;
     line-height: normal;
-    font-weight: 700;
-    margin-right: 10px;
+    font-weight: 600;
+    margin: 3px 10px;
+    background-color: ${({theme}) => theme.colors.back};
+`;
+
+const GoogleBtn = styled.a`
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const Tooltip = styled.span`
+    position: absolute;
+    top: 120%;
+    left: -100%;
+    transform: translateX(-50%);
+    background-color: ${({theme}) => theme.colors.light};
+    color: black;
+    padding: 4px 10px;
+    border-radius: 20px;
+    white-space: nowrap;
+    font-size: 12px;
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.2s ease, visibility 0.2s ease;
+    pointer-events: none;
+    z-index: 10;
+
+    &::after {
+        content: '';
+        position: absolute;
+        bottom: 100%;
+        right: 9%;
+        transform: translateX(-50%);
+        border-width: 5px;
+        border-style: solid;
+        border-color: transparent transparent ${({theme}) => theme.colors.light} transparent;
+    }
+
+`;
+
+
+const StyledGoogleSVG = styled(ReactSVG)`
+    &:hover svg rect {
+        stroke: ${({theme}) => theme.colors.strong};
+    }
+
+    
+`;
+
+const GoogleDiv = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    //margin-right: 40px;
+    cursor: pointer;
+
+    &:hover .tooltip{
+        opacity: 1;
+        visibility: visible;
+    }
 `;
 
 const Header = () => {
@@ -30,14 +91,21 @@ const Header = () => {
 
     return (
         <HeaderStyle>
-            {session && 
-            <>
-                <UserInfo>
-                    <EmailStyle>{session}</EmailStyle>
+            <UserInfo>
+                {session ?
+                <>
+                    <EmailStyle>{session} 😽 </EmailStyle>
                     <Logout />
-                </UserInfo>
-            </>
-            }
+                </>
+                :
+                <GoogleDiv>
+                    <GoogleBtn href="https://planmytodos-api-production.up.railway.app/oauth2/authorization/google">
+                        <Tooltip className="tooltip">Sign In with Google</Tooltip>
+                        <StyledGoogleSVG src="/images/web_light_rd_na.svg"/>
+                    </GoogleBtn>
+                </GoogleDiv>
+                }
+            </UserInfo>
         </HeaderStyle>
     );
 }
--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -2,38 +2,18 @@ import styled from 'styled-components';
 import { ReactSVG } from "react-svg";
 import { CmModal } from './Common/CmModal';
 
-
-const GoogleBtn = styled.a`
-    margin-top: 20px;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    background: transparent;
-    width: 90%;
-    height: 45px;
-    padding-left: 10px;
-    font-size: 16px;
-    font-weight: 700;
-    border: 1px solid #1976D2;
-    color: inherit;
-    background-color: #fff;
-    text-decoration: none;
-
-    &:hover{
-        box-shadow: 1px 1px 1px #1976D2;
-    }
-
-    svg{
-        margin-right: 10px;
-    }
-`;
-
 const Comment = styled.span`
     font-size: 16px;
     font-weight: 700;
-    margin-bottom: 10px;
+    margin-bottom: 40px;
     line-height: normal;
     box-shadow: inset 0 -10px #fbea9d;
+`;
+
+const StyledGoogleBtn = styled(ReactSVG)`
+    &:hover .border {
+        stroke: #fbea9d !important;
+    }
 `;
 
 export const LoginModal = ({isOpen, onRequestClose}) => {
@@ -42,10 +22,9 @@ export const LoginModal = ({isOpen, onRequestClose}) => {
         <CmModal isOpen={isOpen} onRequestClose={onRequestClose}>
             <Comment>Please Login to use our service 😽 !</Comment>
             
-            <GoogleBtn href="https://planmytodos-api-production.up.railway.app/oauth2/authorization/google">
-                <ReactSVG src="/images/google.svg"/>
-                <span>Login with Google</span>
-            </GoogleBtn>
+            <a href="https://planmytodos-api-production.up.railway.app/oauth2/authorization/google">
+                <StyledGoogleBtn src="/images/web_light_rd_ctn.svg"/>
+            </a>
         </CmModal>
     )
 }
